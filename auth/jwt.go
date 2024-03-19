3e39bf6f0592bff951f6d2d9bba3aa86e743223e
Arthur D <a.dranchuk@setplex.com>
2024-03-19 16:10:14 +0300

    fix: rmv debug fmts

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 
@@ -18,44 +17,42 @@ type JWTAuth struct {
 	*gost.JwtConfig
 }
 
-func (a JWTAuth) Auth(r *http.Request, usr users.Store, stg *settings.Settings, srv *settings.Server) (*users.User, error) {
-	fmt.Println("auth jwt")
+func (a JWTAuth) Auth(r *http.Request, usr users.Store, stg *settings.Settings, srv *settings.Server) (u *users.User, err error) {
+	defer func() {
+		if err != nil && u == nil {
+			u, err = JSONAuth{nil}.Auth(r, usr, stg, srv)
+		}
+	}()
+
 	if err := a.Validate(); err != nil {
-		fmt.Println("validate err: ", err)
 		return nil, err
 	}
 
 	rawTkn, err := a.getToken(r)
 	if err != nil {
-		fmt.Println("failed to get token: ", err)
 		return nil, err
 	}
 
 	ja := jwtauth.New(a.Alg, a.PublicKey(), nil)
 	token, err := jwtauth.VerifyToken(ja, rawTkn)
 	if err != nil {
-		fmt.Println("failed to verify token: ", err)
 		return nil, err
 	}
 
 	claims, err := token.AsMap(r.Context())
 	if err != nil {
-		fmt.Println("failed to get token as map: ", err)
 		return nil, err
 	}
 
 	if claims["username"] == nil {
-		fmt.Println("empty username: ", err)
 		return nil, errors.ErrInvalidRequestParams
 	}
 
-	u, err := usr.Get(srv.Root, claims["username"].(string))
+	u, err = usr.Get(srv.Root, claims["username"].(string))
 	if err != nil {
-		fmt.Println("failed to get user from claims: ", err)
 		return nil, err
 	}
 
-	fmt.Printf("jwt auth: %+v; claims: %v\n", u, claims)
 	return u, nil
 }
 
@@ -71,26 +68,20 @@ func (a JWTAuth) getToken(r *http.Request) (string, error) {
 
 	if refererer := r.Header.Get("Referer"); refererer != "" {
 		u, _ := url.Parse(refererer)
-		fmt.Println("referer")
 		return u.Query().Get("token"), nil
 	}
 
-	fmt.Println(r.URL)
-
 	if token = r.URL.Query().Get("token"); token != "" {
-		fmt.Println("url")
 		return token, nil
 	}
 
 	cookie, err := r.Cookie("x-auth")
 	if err == nil && cookie.Value != "" {
-		fmt.Println("x-auth")
 		return cookie.Value, nil
 	}
 
 	cookie, err = r.Cookie("auth")
 	if err == nil && cookie.Value != "" {
-		fmt.Println("auth")
 		return cookie.Value, nil
 	}
 
